faf814e8e85fd7a2c6b5621b585299d8b10cec91
Dmitriy Zhilyaev <zhilyaev@users.noreply.github.com>
2023-11-18 14:08:27 +0100

    Create sa2kubectl.sh

---
+++ sa2kubectl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -o errexit
+
+# cosmetic name
+cluster='prod'
+
+# settings
+server=<server address>
+secret=<sa-secret>
+sa=<app-role>
+ns=<sa-ns>
+
+
+ca=$(kubectl get secret $secret -o=jsonpath='{.data.ca\.crt}')
+token=$(kubectl  get secret $secret -o=jsonpath='{.data.token}' | base64 --decode)
+
+
+echo "
+---
+apiVersion: v1
+kind: Config
+clusters:
+  - name: ${cluster}
+    cluster:
+      certificate-authority-data: ${ca}
+      server: ${server}
+contexts:
+  - name: ${sa}@${cluster}
+    context:
+      cluster: ${cluster}
+      namespace: ${ns}
+      user: ${sa}
+users:
+  - name: ${sa}
+    user:
+      token: ${token}
+current-context: ${sa}@${cluster}
+"
